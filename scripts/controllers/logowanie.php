fe4bac2a3b5e4c7c7658af2b9b2a87c2926575a9
bronco-one <zdzislaw.bronikowski@gmail.com>
2019-12-10 20:41:38 +0100

    No message

--- scripts/controllers/logowanie.php
+++ scripts/controllers/logowanie.php
@@ -17,7 +17,8 @@ class Logowanie extends Controller {
         $user = $_POST['user'];
         $pass = $_POST['pass'];
         if($this->model->login($user, $pass)){
-			$this->view->render("zadania/index");
+        header('Location: ../zadania/index');
+			
         }else{
 			$this->view->render("logowanie/logowanie");
         }
