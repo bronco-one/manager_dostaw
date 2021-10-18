b7766c7e6ba033f2d257ef20e2aa72bd50ef01d6
Zdzisław Bronikowski <zdzislaw.bronikowski@gmail.com>
2021-10-18 19:03:48 +0200

    new commit

--- scripts/index.php
+++ scripts/index.php
@@ -2,7 +2,7 @@
 error_reporting(e_all);
 ini_set('display_errors',1);
 
-require_once 'lib/config.php';
+require_once 'lib/Config.php';
 
 function __autoload($class) {
 	require LIBRARY.$class.'.php';
